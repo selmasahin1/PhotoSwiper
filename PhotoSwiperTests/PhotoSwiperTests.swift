1f1f6a71ce7d2a14fc06e21919e6a1251b7ffa73
SelmaSahin1 <s.sahin@bluewin.ch>
2025-05-04 17:08:59 +0200

    Initial Commit

---
+++ PhotoSwiperTests/PhotoSwiperTests.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoSwiperTests.swift
+//  PhotoSwiperTests
+//
+//  Created by Selma Sahin on 04.05.2025.
+//
+
+import Testing
+@testable import PhotoSwiper
+
+struct PhotoSwiperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
